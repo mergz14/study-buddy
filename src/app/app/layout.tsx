75177f1306a326b077824b0e0f3579a4687a7c11
Merghanis Home <merghanishome@imac.lan>
2026-05-10 19:21:56 +0400

    fix app layout

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,8 +1,3 @@
-import { auth } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
-
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  const { userId } = auth()
-  if (!userId) redirect('/sign-in')
   return <>{children}</>
 }
