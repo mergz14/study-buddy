2478808c4dbcc69106719392b78be8b1232b01f9
Merghanis Home <merghanishome@imac.lan>
2026-05-10 19:53:02 +0400

    fix middleware public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
   publicRoutes: ['/', '/sign-in(.*)', '/sign-up(.*)'],
+  ignoredRoutes: ['/((?!api|trpc))(_next|.+\\.[\\w]+$)', '/api/stripe-webhook'],
 })
 
 export const config = {
